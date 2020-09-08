a0f045e27d8f461ac3ef2d0175eeb1360e428f83
Maciek Sitkowski <msitkowski94@gmail.com>
2020-09-08 19:11:11 +0200

    Fetch data, project counties data on svg

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,11 @@
 import {
-  select
+  select,
+  json,
+  geoPath,
 } from 'd3';
+import {
+  feature
+} from 'topojson-client';
 
 // Constants
 const 
@@ -11,3 +16,28 @@ const
 const svg = select('svg')
   .attr('width', width)
   .attr('height', height);
+
+// Data from freeCodeCamp challenge
+const educationData = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json';
+const countiesData = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json';
+
+// Fetch education and counties data
+Promise.all([
+    json(educationData), 
+    json(countiesData)
+  ])
+  .then(([jsonEducationData, jsonCountiesData]) => {
+    console.log(jsonEducationData);
+    console.log(jsonCountiesData);
+
+    const counties = feature(jsonCountiesData, jsonCountiesData.objects.counties);
+    
+    console.log(counties);
+    const path = geoPath();
+
+    svg.selectAll('path')
+      .data(counties.features)
+      .enter().append("path")
+        .attr("fill", 'black')
+        .attr("d", d => path(d))
+  })
